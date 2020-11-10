e2239e635c402ea503bf0d9c9e011ac0aa0ee442
stephin007 <stephinreji123@gmail.com>
2020-11-10 23:48:12 +0530

    Added .env file for prod.

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const URL = "https://api.openweathermap.org/data/2.5/weather";
-const API_KEY = "8a027e3e80e97e5a9f3c1f13b16a9c60";
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
+// const API_KEY = "8a027e3e80e97e5a9f3c1f13b16a9c60"
 
 export const fetchWeather = async(query) => {
     const {data} = await axios.get(URL, {
